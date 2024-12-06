8aca594b662ea90573af5dba77312286528f20a4
Felipe Lira <filipi6219@gmail.com>
2024-12-06 15:12:49 -0300

    refactor: abstrair operações do banco de dados

--- code/functions/ProcessaDadosIOT/index.js
+++ code/functions/ProcessaDadosIOT/index.js
@@ -1,8 +1,5 @@
-let knex;
-
-const registraMedicaoNoBD = async (medicao) => {
-  await knex('medicoes_silos').insert(medicao);
-}
+const OperacoesBD = require('./operacoesBD');
+let operacoesBD;
 
 const montaRegistroMedicao = ({ dadosDoSilo, volumeAtualDoSilo, idNivelSilo, timestampDaMedicao }) => {
   const idMedicao = crypto.randomUUID();
@@ -17,11 +14,7 @@ const montaRegistroMedicao = ({ dadosDoSilo, volumeAtualDoSilo, idNivelSilo, tim
 
 const defineNivelAtual = async (volumeTotal, volumeAtual) => {
   const percentualVolumeAtual = parseFloat((volumeAtual / volumeTotal).toFixed(2));
-  const nivelAtual = await knex('niveis_de_silo')
-    .select('id_nivel_de_silo')
-    .first()
-    .where('percentual_minimo', '<=', percentualVolumeAtual)
-    .andWhere('percentual_maximo', '>=', percentualVolumeAtual);
+  const nivelAtual = await operacoesBD.obtemNivelDeSiloPeloPercentualDeVolume(percentualVolumeAtual);
   return nivelAtual.id_nivel_de_silo;
 }
 
@@ -31,63 +24,34 @@ const calculaVolumeAtual = (dadosDoSilo, percentualVazioDoSilo) => {
   return volumeFixo + (volumeVariavel * fatorDoVolumeVariavelAtual);
 }
 
-const obtemDadosDoSiloDaPlaca = async (idPlaca) => {
-  return await knex('silos')
-    .select(
-      'id_silo as idSilo',
-      'nome',
-      'volume_fixo_em_m3 as volumeFixo',
-      'volume_variavel_em_m3 as volumeVariavel',
-      knex.raw('(volume_fixo_em_m3 + volume_variavel_em_m3) as volumeTotal'),
-      'id_fazenda as idFazenda',
-      'id_placa_medidora as idPlacaMedidora'
-    )
-    .first()
-    .where({ id_placa_medidora: idPlaca });
-}
-
-const registraInteracaoDaPlaca = async (idPlaca, timestamp) => {
-  await knex('placas_medidoras')
-    .update({ timestamp_ultima_interacao: timestamp })
-    .where({ id_placa_medidora: idPlaca });
-}
-
 const montaKnex = () => {
-  knex = require('knex')({
-    client: 'mysql',
-    connection:{
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-  },
-});
+  operacoesBD = new OperacoesBD();
+  operacoesBD.conecta();
 }
 
 const processaDadosIOT = async (context, dados) => {
-    if (dados.comando == 'PING') {
-      await registraInteracaoDaPlaca(dados.idPlaca, dados.timestamp);
-      return;
-    }
+  if (dados.comando == 'PING') {
+    await operacoesBD.registraInteracaoDaPlaca(dados.idPlaca, dados.timestamp);
+    return;
+  }
 
-    const dadosDoSilo = await obtemDadosDoSiloDaPlaca(dados.idPlaca);
-    if (!dadosDoSilo)
-      throw new Error(`Silo de id ${dados.idPlaca} não encontrado!`);
+  const dadosDoSilo = await operacoesBD.obtemDadosDoSiloDaPlaca(dados.idPlaca);
+  if (!dadosDoSilo)
+    throw new Error(`Silo de id ${dados.idPlaca} não encontrado!`);
 
-    const volumeAtualDoSilo = calculaVolumeAtual(dadosDoSilo, dados.dados.medicao);
-    const idNivelSilo = await defineNivelAtual(dadosDoSilo.volumeTotal, volumeAtualDoSilo);
-    const medicao = montaRegistroMedicao({
-      dadosDoSilo,
-      volumeAtualDoSilo,
-      idNivelSilo,
-      timestampDaMedicao: dados.timestamp
-    });
+  const volumeAtualDoSilo = calculaVolumeAtual(dadosDoSilo, dados.dados.medicao);
+  const idNivelSilo = await defineNivelAtual(dadosDoSilo.volumeTotal, volumeAtualDoSilo);
+  const medicao = montaRegistroMedicao({
+    dadosDoSilo,
+    volumeAtualDoSilo,
+    idNivelSilo,
+    timestampDaMedicao: dados.timestamp
+  });
 
-    await registraMedicaoNoBD(medicao);
-    await registraInteracaoDaPlaca(dados.idPlaca, dados.timestamp);
+  await operacoesBD.registraMedicao(medicao);
+  await operacoesBD.registraInteracaoDaPlaca(dados.idPlaca, dados.timestamp);
 
-    context.log("Operação Completa");
+  context.log("Operação Completa");
 }
 
 module.exports = function (context, IoTHubMessages) {
@@ -103,14 +67,14 @@ module.exports = function (context, IoTHubMessages) {
   });
 
   Promise.all(promises)
-  .then(() => {
-    knex.destroy();
-  })
-  .catch(erro => {
-    context.error("Erro na operação");
-    context.error(erro);
-    knex.destroy();
-  });
+    .then(() => {
+      operacoesBD.desconecta();
+    })
+    .catch(erro => {
+      context.error("Erro na operação");
+      context.error(erro);
+      operacoesBD.desconecta();
+    });
 
   context.done();
 };
---
+++ code/functions/ProcessaDadosIOT/operacoesBD.js
@@ -0,0 +1,54 @@
+class OperacoesBD {
+  conecta() {
+    this._knex = require('knex')({
+      client: 'mysql',
+      connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+    })
+  }
+
+  desconecta() {
+    this._knex.destroy();
+    this._knex = null;
+  }
+
+  async registraInteracaoDaPlaca(idPlaca, timestamp) {
+    await this._knex('placas_medidoras')
+      .update({ timestamp_ultima_interacao: timestamp })
+      .where({ id_placa_medidora: idPlaca });
+  }
+
+  async obtemDadosDoSiloDaPlaca(idPlaca) {
+    return await this._knex('silos')
+      .select(
+        'id_silo as idSilo',
+        'nome',
+        'volume_fixo_em_m3 as volumeFixo',
+        'volume_variavel_em_m3 as volumeVariavel',
+        this._knex.raw('(volume_fixo_em_m3 + volume_variavel_em_m3) as volumeTotal'),
+        'id_fazenda as idFazenda',
+        'id_placa_medidora as idPlacaMedidora'
+      )
+      .first()
+      .where({ id_placa_medidora: idPlaca });
+  }
+
+  async obtemNivelDeSiloPeloPercentualDeVolume(percentual) {
+    return await this._knex('niveis_de_silo')
+      .select('id_nivel_de_silo')
+      .first()
+      .where('percentual_minimo', '<=', percentual)
+      .andWhere('percentual_maximo', '>=', percentual);
+  }
+
+  async registraMedicao(medicao) {
+    await this._knex('medicoes_silos').insert(medicao);
+  }
+}
+
+module.exports = OperacoesBD;
