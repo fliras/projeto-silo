c9fd3ddaf1f225ef92a29c9d28e38b3b9216fa51
Felipe Lira <filipi6219@gmail.com>
2025-05-02 19:41:17 -0300

    feat: add queries para utilização no powerapps

---
+++ code/database/docker/queries_powerapps.sql
@@ -0,0 +1,10 @@
+/* obtém a última medição de um silo de id X */
+select * from vw_medicoes
+	where id_silo = X
+    order by timestamp_medicao desc
+    limit 1;
+    
+/* obtém as medições de um silo de id X nos últimos Y dias */
+select * from vw_medicoes
+    where id_silo = X AND timestamp_medicao >= DATE_SUB(CURRENT_DATE, INTERVAL Y DAY)
+    ORDER BY timestamp_medicao;
---
+++ code/database/docker/vw_medicoes.sql
@@ -0,0 +1,13 @@
+CREATE VIEW `vw_medicoes` AS
+select `s`.`id_silo`,
+       `ms`.`timestamp_medicao`,
+       `ns`.`nivel`,
+       (`ns`.`percentual_minimo` * 100) AS `percentual_minimo`,
+       (`ns`.`percentual_maximo` * 100) AS `percentual_maximo`,
+       (`s`.`volume_total` * `ns`.`percentual_minimo`) AS `volume_minimo`,
+       (`s`.`volume_total` * `ns`.`percentual_maximo`) AS `volume_maximo`,
+       (`s`.`densidade_armazenada` * `s`.`volume_total` * `ns`.`percentual_minimo`) AS `peso_minimo`,
+       (`s`.`densidade_armazenada` * `s`.`volume_total` * `ns`.`percentual_maximo`) AS `peso_maximo` 
+       from `silodb`.`medicoes_silos` `ms` 
+       join `silodb`.`niveis_de_silo` `ns` on `ns`.`id_nivel_de_silo` = `ms`.`id_nivel_de_silo`
+       join `silodb`.`silos` `s` on `ms`.`id_silo` = `s`.`id_silo`
