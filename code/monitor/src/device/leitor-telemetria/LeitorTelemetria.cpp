0f271bcfc87415822a701449ea885caf640dea65
Felipe Lira <filipi6219@gmail.com>
2025-05-03 13:43:24 -0300

    feat: implementa nova lógica para ler telemetrias

--- code/monitor/src/device/leitor-telemetria/LeitorTelemetria.cpp
+++ code/monitor/src/device/leitor-telemetria/LeitorTelemetria.cpp
@@ -2,31 +2,13 @@
 
 LeitorTelemetria::LeitorTelemetria(char *IDPlaca, bool deveMockarLDR) : IDPlaca(IDPlaca), deveMockarLDR(deveMockarLDR) {};
 
-String LeitorTelemetria::obtemLeitura()
+int LeitorTelemetria::obtemLeitura()
 {
-  float porcentagem = obtemPorcentagemDoSensor();
-  return montaPayload(porcentagem);
+  return deveMockarLDR ? mockaValorDoSensor() : analogRead(PINO_ANALOGICO_DO_LDR);
 }
 
-float LeitorTelemetria::obtemPorcentagemDoSensor()
-{
-  int valorDoSensor = deveMockarLDR ? mockaValorDoSensor() : analogRead(PINO_ANALOGICO_DO_LDR);
-  Logger.Info("Valor coletado do LDR: " + String(valorDoSensor));
-  return remapeiaLDRParaPorcentagem(valorDoSensor, VALOR_MIN_DO_LDR, VALOR_MAX_DO_LDR, 0, 100);
-}
-
+// método que gera um valor aleatório entre os valores mínimos e máximos do sensor
 int LeitorTelemetria::mockaValorDoSensor() {
   srand((unsigned) time(NULL));
-  return VALOR_MIN_DO_LDR + (rand() % VALOR_MAX_DO_LDR);
-}
-
-float LeitorTelemetria::remapeiaLDRParaPorcentagem(float x, float in_min, float in_max, float out_min, float out_max) {
-  float percentual = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-  float percentualComDuasCasas = round(percentual * 100) / 100;
-  return percentualComDuasCasas;
-}
-
-String LeitorTelemetria::montaPayload(float medicao)
-{
-  return "{\"medicao\": " + String(medicao) + "}";
+  return VALOR_MIN_DO_LDR + ((rand() % (VALOR_MAX_DO_LDR - VALOR_MIN_DO_LDR)));
 }
--- code/monitor/src/device/leitor-telemetria/LeitorTelemetria.h
+++ code/monitor/src/device/leitor-telemetria/LeitorTelemetria.h
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include "src/device/logger-serial/LoggerSerial.h"
 
-#define VALOR_MIN_DO_LDR 0
-#define VALOR_MAX_DO_LDR 4095
+#define VALOR_MIN_DO_LDR 300
+#define VALOR_MAX_DO_LDR 2800
 #define PINO_ANALOGICO_DO_LDR 34
 
 class LeitorTelemetria
@@ -10,12 +10,9 @@ class LeitorTelemetria
   private:
     char* IDPlaca;
     bool deveMockarLDR;
-    float obtemPorcentagemDoSensor();
     int mockaValorDoSensor();
-    float remapeiaLDRParaPorcentagem(float x, float in_min, float in_max, float out_min, float out_max);
-    String montaPayload(float medicao);
   
   public:
     LeitorTelemetria(char* IDPlaca, bool deveMockarLDR);
-    String obtemLeitura();
+    int obtemLeitura();
 };
