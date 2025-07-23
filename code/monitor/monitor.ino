2c4e672ac0ba5f5f53c0cd05208ce574cbfffc43
Felipe Lira <filipi6219@gmail.com>
2025-07-23 11:38:38 -0300

    refactor: quebra fluxo de execução em funções menores

--- code/monitor/monitor.ino
+++ code/monitor/monitor.ino
@@ -10,7 +10,6 @@ static GerenciadorIoTHub gerenciadorIoTHub(CFG_IOTHUB_FQDN, CFG_IOTHUB_URI_MQTT,
 static LeitorTelemetria leitorTelemetria(CFG_IOTHUB_DEVICE_ID, CFG_DEVE_MOCKAR_VALOR_DO_SENSOR);
 
 static unsigned long ultimaContagemDeMilissegundos = 0;
-static bool primeiraExecucao = true;
 static int contadorDeEnvio = CFG_MULTIPLO_DE_ENVIO_DE_TELEMETRIA;
 
 void setup()
@@ -18,6 +17,7 @@ void setup()
   gerenciadorWiFi.conecta();
   GerenciadorDataHora::defineTimeZone();
   gerenciadorIoTHub.configura();
+  executaCiclo();
 }
 
 void loop()
@@ -32,26 +32,31 @@ void loop()
     gerenciadorIoTHub.configura();
   }
 
-  bool iniciouCicloDeEnvio = ((unsigned long)(millis() - ultimaContagemDeMilissegundos) > CFG_INTERVALO_ENVIO_MSG_EM_MILISSEGS);
-  if (primeiraExecucao || iniciouCicloDeEnvio)
+  unsigned long intervaloDesdeUltimoCicloEmMilissegundos = (unsigned long)(millis() - ultimaContagemDeMilissegundos);
+  if (intervaloDesdeUltimoCicloEmMilissegundos >= CFG_INTERVALO_ENVIO_MSG_EM_MILISSEGS)
   {
-    Logger.Info("Iniciando ciclo de envio...");
-    int valorTelemetria = leitorTelemetria.obtemLeitura();
-    Logger.Info("Valor lido do sensor: " + String(valorTelemetria));
-
-    bool deveEnviarTelemetria = CFG_DEVE_ENVIAR_MSG && contadorDeEnvio == CFG_MULTIPLO_DE_ENVIO_DE_TELEMETRIA;
-    if (deveEnviarTelemetria)
-    {
-      trataEnvioDeTelemetria(valorTelemetria);
-    }
-    else if (CFG_DEVE_ENVIAR_MSG)
-    {
-      trataEnvioDePing();
-    }
-    
-    preparaProximoCicloDeEnvio();
-    Logger.Info("Ciclo finalizado.");
+    executaCiclo();
   }
+}
+
+void executaCiclo()
+{
+  Logger.Info("Iniciando ciclo...");
+  int valorTelemetria = leitorTelemetria.obtemLeitura();
+  Logger.Info("Valor lido do sensor: " + String(valorTelemetria));
+
+  bool enviaTelemetriaNesseCiclo = contadorDeEnvio == CFG_MULTIPLO_DE_ENVIO_DE_TELEMETRIA;
+  if (CFG_DEVE_ENVIAR_MSG && enviaTelemetriaNesseCiclo)
+  {
+    trataEnvioDeTelemetria(valorTelemetria);
+  }
+  else if (CFG_DEVE_ENVIAR_MSG)
+  {
+    trataEnvioDePing();
+  }
+
+  ultimaContagemDeMilissegundos = millis();
+  Logger.Info("Ciclo finalizado.");
 }
 
 void trataEnvioDeTelemetria(int valorTelemetria)
@@ -66,10 +71,4 @@ void trataEnvioDePing()
   Logger.Info("Envio de ping! Contador: " + String(contadorDeEnvio));
   gerenciadorIoTHub.enviaPing();
   contadorDeEnvio++;
-}
-
-void preparaProximoCicloDeEnvio()
-{
-  ultimaContagemDeMilissegundos = millis();
-  primeiraExecucao = false;
 }
