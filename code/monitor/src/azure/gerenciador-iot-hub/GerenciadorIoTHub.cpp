dfd1a80d0c6f569b4db0a603e10e56d03023d9d8
Felipe Lira <filipi6219@gmail.com>
2025-07-23 17:46:24 -0300

    docs: comenta arquivos do GerenciadorIoTHub

--- code/monitor/src/azure/gerenciador-iot-hub/GerenciadorIoTHub.cpp
+++ code/monitor/src/azure/gerenciador-iot-hub/GerenciadorIoTHub.cpp
@@ -1,50 +1,77 @@
+/*
+* GerenciadorIoTHub.cpp:
+* Implementação das funcionalidades definidas em GerenciadorIoTHub.h
+*/
+
+// Inclusão dos Headers
 #include "GerenciadorIoTHub.h"
 
+
+/// @brief Implementação do construtor, que inicializa os atributos da classe.
+/// @param fqdn Domínio FQDN do serviço IoT Hub configurado no Azure.
+/// @param uriMQTT endereço MQTT configurado no Azure.
+/// @param deviceID ID do dispositivo IoT criado no Azure.
+/// @param deviceKey chave do dispositivo IoT criado no Azure.
 GerenciadorIoTHub::GerenciadorIoTHub(char *fqdn, char *uriMQTT, char *deviceID, char *deviceKey)
 {
   this->gerenciadorDoClient = new GerenciadorClientIoTHub(fqdn, deviceID);
   this->clientMQTT = new ClientMQTTAzure(uriMQTT);
 }
 
+
+/// @brief Implementação do método que configura os componentes que interagem com partes específicas do Azure.
 void GerenciadorIoTHub::configura()
 {
+  // inicia o client do IoT Hub caso não esteja em funcionamento.
   if (!gerenciadorDoClient->foiInicializado())
     gerenciadorDoClient->inicializa();
 
-  if (!sessaoEstaValidada())
+  // caso a sessão não esteja em vigor, tenta gerar outro token.
+  if (!sessaoEstaValidada() && !gerenciadorDoClient->geraToken(DURACAO_SAS_TOKEN_EM_MINUTOS))
   {
-    if (!gerenciadorDoClient->geraToken(DURACAO_SAS_TOKEN_EM_MINUTOS))
-    {
-      Logger.Error("Falha ao gerar um novo token");
-      return;
-    }
+    Logger.Error("Falha ao gerar um novo token ao revalidar a sessao");
+    return;
   }
 
+  // caso o client MQTT já esteja operando, ele é encerrado para que seja iniciado novamente.
   if (clientMQTT->verificaSeEstaConfigurado())
     clientMQTT->encerra();
   (void)clientMQTT->configura(gerenciadorDoClient->obtemID(), gerenciadorDoClient->obtemUsername(), gerenciadorDoClient->obtemToken());
 }
 
+
+/// @brief Implementação do método que verifica se a sessão entre Esp32 e Azure está em vigor, checando a situação do token.
+/// @return true: sessão validada; false: sessão não validada.
 bool GerenciadorIoTHub::sessaoEstaValidada()
 {
   return gerenciadorDoClient->tokenValido();
 }
 
+
+/// @brief Implementação do método que envia ao Azure uma mensagem em JSON com dados de medição.
+/// @param valorTelemetria Valor coletado pelo sensor.
 void GerenciadorIoTHub::enviaTelemetria(int valorTelemetria)
 {
-  String payloadTelemetria = "{\"medicao\": " + String(valorTelemetria) + "}";
+  String payloadTelemetria = "{\"medicao\": " + String(valorTelemetria) + "}"; // Ex: {"medicao": 123}
   String mensagem = montaMensagem("MEDICAO", payloadTelemetria);
   clientMQTT->publica(gerenciadorDoClient->obtemTopicoDaTelemetria(), mensagem);
 }
 
+
+/// @brief Implementação do método que envia pings ao Azure, para indicar que o Esp32 está em atividade.
 void GerenciadorIoTHub::enviaPing()
 {
   String mensagem = montaMensagem("PING", "{}");
   clientMQTT->publica(gerenciadorDoClient->obtemTopicoDaTelemetria(), mensagem);
 }
 
+
+/// @brief Método para montar mensagens em JSON, em um formato esperado pelo Azure.
+/// @param comando Tipo da mensagem, podendo ser "MEDICAO" ou "PING".
+/// @param payload Objeto JSON em String com o conteúdo da mensagem que será enviada ao Azure.
+/// @return 
 String GerenciadorIoTHub::montaMensagem(String comando, String payload)
 {
-  // {"idPlaca": "", "comando": "", "timestamp": "", dados: {}}
+  // Ex: {"idPlaca": "", "comando": "", "timestamp": "", dados: {}}
   return "{\"idPlaca\":\""+ String(gerenciadorDoClient->obtemID()) + "\",\"comando\":\"" + comando + "\",\"timestamp\":\"" + GerenciadorDataHora::obtemTimestampAtual() + "\",\"dados\":" + payload + "}";
 }
--- code/monitor/src/azure/gerenciador-iot-hub/GerenciadorIoTHub.h
+++ code/monitor/src/azure/gerenciador-iot-hub/GerenciadorIoTHub.h
@@ -1,14 +1,25 @@
+/*
+* GerenciadorIoTHub.h:
+* Header do GerenciadorIoTHub, classe central para mediar a interação entre Esp32 e Azure IoT Hub.
+*/
+
 #ifndef AZ_IOT_HUB_SERVICE_H
 #define AZ_IOT_HUB_SERVICE_H
 
+// Inclusão dos Headers
 #include "Arduino.h"
 #include "src/azure/gerenciador-client-iot-hub/GerenciadorClientIoTHub.h"
 #include "src/azure/client-mqtt/ClientMQTTAzure.h"
 #include "src/device/logger-serial/LoggerSerial.h"
 #include "src/device/gerenciador-data-hora/GerenciadorDataHora.h"
 
+
+// Constantes utilizadas
 #define DURACAO_SAS_TOKEN_EM_MINUTOS 60
 
+
+/// @brief Protótipo do GerenciadorIoTHub, que deve gerenciar a conexão com o Azure e enviar
+/// mensagens com as medições extraídas ou mensagens de Ping, para indicar atividade.
 class GerenciadorIoTHub
 {
   private:
