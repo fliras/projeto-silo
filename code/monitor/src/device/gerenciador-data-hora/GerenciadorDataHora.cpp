c15ad3dfc3de5952179b8afb2a9b87e5f9514be6
Felipe Lira <filipi6219@gmail.com>
2025-07-23 15:24:56 -0300

    docs: comenta arquivos do GerenciadorDataHora

--- code/monitor/src/device/gerenciador-data-hora/GerenciadorDataHora.cpp
+++ code/monitor/src/device/gerenciador-data-hora/GerenciadorDataHora.cpp
@@ -1,11 +1,26 @@
+/*
+* GerenciadorDataHora.cpp:
+* Implementa as funcionalidades definidas no GerenciadorDataHora.h
+*/
+
+// Inclusão dos Headers
 #include "GerenciadorDataHora.h"
 
+
+/// @brief Implementação do Construtor
 GerenciadorDataHora::GerenciadorDataHora() {}
 
+
+/// @brief Implementação do método que define o TimeZone utilizado no Esp32,
+/// por meio de servidores NTP.
+// OBS: Como a configuração de Data e Hora é feita via internet, o fluxo
+// de inicialização do software pode ficar preso aqui caso a rede não tenha uma conexão estável.
 void GerenciadorDataHora::defineTimeZone()
 {
   // o zero representa ausência de horário de verão
   configTime(GMT_OFFSET_SECS, 0, SERVIDORES_NTP);
+
+  // enquanto o TimeZone estiver incorreto, continuará tentando configurar.
   time_t timestampAtual = time(NULL);
   while (timestampAtual < DIA_13_NOV_2017_EM_UNIX_TIME)
   {
@@ -14,27 +29,30 @@ void GerenciadorDataHora::defineTimeZone()
   }
 }
 
+
+/// @brief Implementação do método que obtém a data e hora atual no Esp32.
+/// @return data e hora atuais no seguinte formato: "aaaa-mm-dd hh:mm:ss"
 String GerenciadorDataHora::obtemTimestampAtual()
 {
-  struct tm tmstruct;
-  getLocalTime(&tmstruct);
-  String timestampString = "";
+  struct tm timestampBruto;
+  getLocalTime(&timestampBruto);
 
-  timestampString += String(tmstruct.tm_year + ANO_INICIAL_EPOCH_UNIX);
-  timestampString += "-" + String(tmstruct.tm_mon + 1);
-  timestampString += "-" + String(tmstruct.tm_mday) + " ";
+  String timestampFormatado = "";
+  timestampFormatado += String(timestampBruto.tm_year + ANO_INICIAL_EPOCH_UNIX);
+  timestampFormatado += "-" + String(timestampBruto.tm_mon + 1);
+  timestampFormatado += "-" + String(timestampBruto.tm_mday) + " ";
 
-  if (tmstruct.tm_hour < 10)
-    timestampString += "0";
-  timestampString += String(tmstruct.tm_hour) + ":";
+  if (timestampBruto.tm_hour < 10)
+    timestampFormatado += "0";
+  timestampFormatado += String(timestampBruto.tm_hour) + ":";
 
-  if (tmstruct.tm_min < 10)
-    timestampString += "0";
-  timestampString += String(tmstruct.tm_min) + ":";
+  if (timestampBruto.tm_min < 10)
+    timestampFormatado += "0";
+  timestampFormatado += String(timestampBruto.tm_min) + ":";
 
-  if (tmstruct.tm_sec < 10)
-    timestampString += "0";
-  timestampString += String(tmstruct.tm_sec);
+  if (timestampBruto.tm_sec < 10)
+    timestampFormatado += "0";
+  timestampFormatado += String(timestampBruto.tm_sec);
 
-  return timestampString;
+  return timestampFormatado;
 }
--- code/monitor/src/device/gerenciador-data-hora/GerenciadorDataHora.h
+++ code/monitor/src/device/gerenciador-data-hora/GerenciadorDataHora.h
@@ -1,18 +1,27 @@
-#ifndef TIME_CONFIGURATOR_H
+/*
+* GerenciadorDataHora.h:
+* Header do GerenciadorDataHora, utilizado para inicializar e coletar data e hora utilizadas na execução do Esp32
+*/
 
+#ifndef TIME_CONFIGURATOR_H
 #define TIME_CONFIGURATOR_H
 
+
+// Inclusão dos headers
 #include <Arduino.h>
 #include <time.h>
 #include <cstdlib>
 
+
+// Constantes utilizadas
+#define SERVIDORES_NTP "pool.ntp.org", "time.nist.gov"
 #define ANO_INICIAL_EPOCH_UNIX 1900
 #define DIA_13_NOV_2017_EM_UNIX_TIME 1510592825
-#define SERVIDORES_NTP "pool.ntp.org", "time.nist.gov"
-
 #define TIME_ZONE_GMT -3
 #define GMT_OFFSET_SECS (TIME_ZONE_GMT * 3600)
 
+
+/// @brief Protótipo da classe GerenciadorDataHora
 class GerenciadorDataHora
 {
   public:
