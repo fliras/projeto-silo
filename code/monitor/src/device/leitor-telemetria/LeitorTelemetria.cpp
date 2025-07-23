931600cb89341477a9a49e0c3ff78d7f4a03b620
Felipe Lira <filipi6219@gmail.com>
2025-07-23 14:52:50 -0300

    docs: comenta arquivos do LeitorTelemetria

--- code/monitor/src/device/leitor-telemetria/LeitorTelemetria.cpp
+++ code/monitor/src/device/leitor-telemetria/LeitorTelemetria.cpp
@@ -1,13 +1,27 @@
+/*
+* LeitorTelemetria.cpp:
+* Implementa as funcionalidades definidas no LeitorTelemetria.h
+*/
+
+// Inclusão dos Headers
 #include "LeitorTelemetria.h"
 
+
+/// @brief Implementação do construtor, que define se os dados devem ser lidos do sensor ou gerados aleatóriamente.
+/// @param deveMockarSensor Indica se as leituras devem retornar dados reais do sensor ou valores aleatórios.
 LeitorTelemetria::LeitorTelemetria(bool deveMockarSensor) : deveMockarSensor(deveMockarSensor) {};
 
+
+/// @brief Implementação do método que coleta dados do sensor ou um valor gerado aleatoriamente.
+/// @return Valor que corresponde a medição atual. 
 int LeitorTelemetria::obtemLeitura()
 {
   return deveMockarSensor ? mockaValorDoSensor() : analogRead(PINO_ANALOGICO_DO_SENSOR);
 }
 
-// método que gera um valor aleatório entre os valores mínimos e máximos do sensor
+
+/// @brief Implementação do método que gera um valor aleatório entre os valores mínimos e máximos do sensor, definidos no Header.
+/// @return Número gerado aleatoriamente.
 int LeitorTelemetria::mockaValorDoSensor() {
   srand((unsigned) time(NULL));
   return VALOR_MIN_DO_SENSOR + ((rand() % (VALOR_MAX_DO_SENSOR - VALOR_MIN_DO_SENSOR)));
--- code/monitor/src/device/leitor-telemetria/LeitorTelemetria.h
+++ code/monitor/src/device/leitor-telemetria/LeitorTelemetria.h
@@ -1,10 +1,20 @@
+/*
+* LeitorTelemetria.h:
+* Header do LeitorTelemetria, utilizado para pré-declarar os componentes utilizados na coleta de dados do sensor.
+*/
+
+// Inclusão de bibliotecas e headers de outros componentes.
 #include <Arduino.h>
 #include "src/device/logger-serial/LoggerSerial.h"
 
+
+// Constantes utilizadas
 #define VALOR_MIN_DO_SENSOR 300
 #define VALOR_MAX_DO_SENSOR 2800
 #define PINO_ANALOGICO_DO_SENSOR 34
 
+
+/// @brief Protótipo da classe LeitorTelemetria, utilizada para coletar dados reais do sensor ou simulá-los.
 class LeitorTelemetria
 {
   private:
