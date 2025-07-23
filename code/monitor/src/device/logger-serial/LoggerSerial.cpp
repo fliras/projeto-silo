4a24c97c9cc7fbbdf2d9c34a2a608099ec4a09d3
Felipe Lira <filipi6219@gmail.com>
2025-07-23 14:33:39 -0300

    docs: comenta arquivos do LoggerSerial

--- code/monitor/src/device/logger-serial/LoggerSerial.cpp
+++ code/monitor/src/device/logger-serial/LoggerSerial.cpp
@@ -1,10 +1,20 @@
+/*
+* LoggerSerial.cpp:
+* Este é o arquivo que implementa as funcionalidades definidas em LoggerSerial.h
+*/
+
+// Importação dos Headers utilizados
 #include "LoggerSerial.h"
 
+
+/// @brief Implementação do construtor, que inicializa a comunicação via serial.
 LoggerSerial::LoggerSerial()
 {
   Serial.begin(BAUD_RATE_DO_SERIAL);
 }
 
+/// @brief Implementação do método que escreve informações via serial.
+/// @param mensagem Conteúdo do log que será escrito.
 void LoggerSerial::Info(String mensagem)
 {
   Serial.print(GerenciadorDataHora::obtemTimestampAtual());
@@ -12,6 +22,8 @@ void LoggerSerial::Info(String mensagem)
   Serial.println(mensagem);
 }
 
+/// @brief Implementação do método que escreve mensagens de erro via serial.
+/// @param mensagem Conteúdo do log que será escrito.
 void LoggerSerial::Error(String mensagem)
 {
   Serial.print(GerenciadorDataHora::obtemTimestampAtual());
@@ -19,4 +31,5 @@ void LoggerSerial::Error(String mensagem)
   Serial.println(mensagem);
 }
 
+/// @brief Instância global exportada ao importar o Header do LoggerSerial
 LoggerSerial Logger;
--- code/monitor/src/device/logger-serial/LoggerSerial.h
+++ code/monitor/src/device/logger-serial/LoggerSerial.h
@@ -1,14 +1,21 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// SPDX-License-Identifier: MIT
+/*
+* LoggerSerial.h:
+* Header do LoggerSerial, utilizado para pré-declarar os componentes utilizados na escrita via Serial.
+*/
 
 #ifndef SERIALLOGGER_H
 #define SERIALLOGGER_H
 
+
+// Importação de bibliotecas e headers de outros componentes
 #include <Arduino.h>
 #include "src/device/gerenciador-data-hora/GerenciadorDataHora.h"
 
+
+// Constantes utilizadas pelo componente
 #define BAUD_RATE_DO_SERIAL 115200
 
+/// @brief Protótipo da classe do Logger que será implementada, atuando na escrita de informações e erros via Serial.
 class LoggerSerial
 {
 public:
@@ -17,6 +24,7 @@ public:
   void Error(String mensagem);
 };
 
+// Já exporta uma instância criada do LoggerSerial assim que o Header é importado por outro componente
 extern LoggerSerial Logger;
 
 #endif // SERIALLOGGER_H
