d6acf0c03806f412dbe7fbcff85ec979970020dc
Felipe Lira <filipi6219@gmail.com>
2025-07-23 14:24:20 -0300

    docs: comenta arquivo central do monitor de silo

--- code/monitor/monitor.ino
+++ code/monitor/monitor.ino
@@ -1,3 +1,9 @@
+/*
+* monitor.ino:
+* Este é o arquivo central da aplicação, no qual todos os componentes são montados e inicializados.
+*/
+
+// importação dos componentes e constantes
 #include "parametros.h"
 #include "src/device/logger-serial/LoggerSerial.h"
 #include "src/device/gerenciador-wifi/GerenciadorWiFi.h"
@@ -5,13 +11,20 @@
 #include "src/device/leitor-telemetria/LeitorTelemetria.h"
 #include "src/azure/gerenciador-iot-hub/GerenciadorIoTHub.h"
 
+
+// instanciação dos componentes utilizados para gerenciar Wi-Fi, conexão com o Azure IoT Hub e coleta de telemetrias (medições).
 static GerenciadorWiFi gerenciadorWiFi(CFG_WIFI_SSID_AP, CFG_WIFI_SENHA_AP);
 static GerenciadorIoTHub gerenciadorIoTHub(CFG_IOTHUB_FQDN, CFG_IOTHUB_URI_MQTT, CFG_IOTHUB_DEVICE_ID, CFG_IOTHUB_DEVICE_KEY);
 static LeitorTelemetria leitorTelemetria(CFG_IOTHUB_DEVICE_ID, CFG_DEVE_MOCKAR_VALOR_DO_SENSOR);
 
+
+// variáveis de controle do fluxo de execução.
 static unsigned long ultimaContagemDeMilissegundos = 0;
 static int contadorDeEnvio = CFG_MULTIPLO_DE_ENVIO_DE_TELEMETRIA;
 
+
+/// @brief Método de inicialização do Esp32, que conecta na rede Wi-Fi, configura data e hora, conecta-se ao IoT Hub e inicia
+/// o primeiro ciclo de execução.
 void setup()
 {
   gerenciadorWiFi.conecta();
@@ -20,6 +33,9 @@ void setup()
   executaCiclo();
 }
 
+
+/// @brief Método principal do ciclo de vida da aplicação, que a cada execução verifica a estabilidade da conexão e executa
+/// um ciclo de extração/envio de dados periodicamente.
 void loop()
 {
   if (!gerenciadorWiFi.estaConectado())
@@ -39,6 +55,8 @@ void loop()
   }
 }
 
+
+/// @brief Método que representa um ciclo de execução do monitor, que coleta dados do sensor e os envia ao Azure quando necessário.
 void executaCiclo()
 {
   Logger.Info("Iniciando ciclo...");
@@ -59,16 +77,19 @@ void executaCiclo()
   Logger.Info("Ciclo finalizado.");
 }
 
+/// @brief Método que efetivamente envia dados ao Azure.
+/// @param valorTelemetria valor extraído do sensor que será enviado ao Azure.
 void trataEnvioDeTelemetria(int valorTelemetria)
 {
   Logger.Info("Envio de telemetria! Contador: " + String(contadorDeEnvio));
   gerenciadorIoTHub.enviaTelemetria(valorTelemetria);
-  contadorDeEnvio = 1;
+  contadorDeEnvio = 1; // Com o envio de uma medição, a contagem é resetada para dar lugar às mensagens de Ping.
 }
 
+/// @brief Método que envia mensagens de Ping ao Azure, para indicar que o Esp32 está operante.
 void trataEnvioDePing()
 {
   Logger.Info("Envio de ping! Contador: " + String(contadorDeEnvio));
   gerenciadorIoTHub.enviaPing();
-  contadorDeEnvio++;
+  contadorDeEnvio++; // A cada ping o contador é aumentado para que, em algum momento, uma medição seja enviada.
 }
